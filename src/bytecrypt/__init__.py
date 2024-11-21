4dc466d3f0c40d9d5ecb5c860712d4ade8ded4f8
milardich <stjepan.milardic1@gmail.com>
2024-11-21 17:22:21 +0100

    added script for building executable

--- src/bytecrypt/__init__.py
+++ src/bytecrypt/__init__.py
@@ -1,4 +1,7 @@
 import tomllib
+import os
+import sys
+
 
 from .bytecrypt import (
     encrypt_bytes, 
@@ -14,8 +17,24 @@ from .bytecrypt import (
 __version__ = "0.0.0"
 data = None
 
-with open("pyproject.toml", "rb") as f:
+def get_resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS 
+    except AttributeError:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+# Example usage
+pyproject_path = get_resource_path("pyproject.toml")
+
+
+with open(pyproject_path, "rb") as f:
     data = tomllib.load(f)
+    
 
 __version__ = data['project']['version']
 print("\nversion: " + str(__version__))
+
+
+def get_version():
+    return str(__version__)
